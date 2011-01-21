1c4422ac22ffe879aceb1296608b3eaa440f1237
Phil Rigovanov <philippe.rigovanov@yandex.com>
2011-01-21 04:47:07 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rigovanov::Application.config.secret_token = 'd9bb24de5501eeabbc88afae0b062ded4a0ba92e9e8543ea4aa68a079c0a2a34f3a10fb9128d59841d63a915ad9a9e6d15549dc405ac5951e86e62a0efcdb1d2'
